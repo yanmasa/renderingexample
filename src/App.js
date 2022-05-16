9048d28d636c6cffc013433ab0329706d8b348f2
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-16 10:57:34 +0900

    useState,useCallback,useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -11,13 +11,19 @@ export default function App() {
   const onClickOpen = () => {
     return setOpen(!open);
   };
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText}></input>
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
